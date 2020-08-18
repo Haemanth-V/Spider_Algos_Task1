c5c362e7a333fdd9331183cb25e1c27ca30e2711
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2020-08-18 10:51:13 +0530

    Update Task 1(D).cpp

--- Task 1(D).cpp
+++ Task 1(D).cpp
@@ -7,10 +7,10 @@ int main(){
 	int n,r,x,y,final_r;
 	cin>>n>>r>>x>>y;
 
-    //Dynamic arrays to store contest and SCN eating information
+        //Dynamic arrays to store contest and SCN eating information
 	int *contest = new int[n];
 	int *scn = new int[n];
-    final_r = r; //Final rating
+        final_r = r; //Final rating
 
 	for(int i=0; i<n; i++)
 	   cin>>contest[i];
@@ -18,18 +18,18 @@ int main(){
 	   cin>>scn[i];
 	   
 	for(int i=0;i<n;i++){
-		if(contest[i]==1){
-			if(scn[i]==1)
-			  final_r += x;  //Increasing rating as he would perform well in contest
-			else  
-			  final_r -= y;
-		}
-	}
+	    if(contest[i]==1){
+	 	if(scn[i]==1)
+	   	   final_r += x;  //Increasing rating as he would perform well in contest
+		else  
+		   final_r -= y;
+	     }
+	 }
 	
-	if(final_r > r)
-	   cout<<"promoted";
-	else if(final_r < r)
-	   cout<<"demoted";
-	else
-	   cout<<"no change";
+	 if(final_r > r)
+	    cout<<"promoted";
+	 else if(final_r < r)
+	    cout<<"demoted";
+	 else
+	    cout<<"no change";
 }
