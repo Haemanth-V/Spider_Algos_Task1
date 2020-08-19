226dbd1eb803c2b37cb9c550b93b43bb63436687
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2020-08-19 15:18:46 +0530

    Update Task 1(C).cpp
    Updated Task 1(C)

--- Task 1(C).cpp
+++ Task 1(C).cpp
@@ -11,66 +11,66 @@ int main(){
 	int arr1[] = {16,10,7,9,43,8};
 	int *a,product,result[6];
 	a = new int[2];
-	for(int i=0;i<=4;i++){
+        for(int i=0;i<=4;i++){
 	 
 	    //query asked using positions (not indices)
 	    printf("%d %d\n",i+1,i+2);
 	    fflush(stdout);
-		scanf("%d",&product);
-		int k=0;
+	    scanf("%d",&product);
+    	    int k=0;
 		
-		//The resulting array is considered as two halves with size 3 each
-		for(int j=0;j<6;j++){
-			if(product%arr1[j]==0){
-				a[k] = arr1[j];
-				k++;
-			}
-			if(k==2)
-			  break;
+  	    //The resulting array is considered as two halves with size 3 each
+   	    for(int j=0;j<6;j++){
+		if(product%arr1[j]==0){
+	  	   a[k] = arr1[j];
+		   k++;
+		}
+		if(k==2)
+		   break;
 	    }
 	    
 	    //The only case where 16 interferes (undesirable)
 	    if(product == 80){
 	    	a[0] = 8;
 	    	a[1] = 10;
-		}
+   	    }
         
 		
-		//Storing the first two numbers disregarding their actual order	
-		if(i%3==0){
-	   		result[i]=a[0];
+  	    //Storing the first two numbers disregarding their actual order	
+	    if(i%3==0){
+		result[i]=a[0];
 	    	result[i+1]=a[1];
 	    }
     	
-    	//Finding the order in which the 3 numbers 
-		// (of one half of the resultant array) appear and storing them 
-		else{
-	   		if(result[i]==a[0])
-	    	  result[i+1] = a[1];
+       	    //Finding the order in which the 3 numbers 
+	    // (of one half of the resultant array) appear and storing them 
+	    else{
+		if(result[i]==a[0])
+	    	   result[i+1] = a[1];
     		else if(result[i]==a[1])
-	   		  result[i+1] = a[0];
-	   		else{
-	   			int t = result[i-1];
-	   			result[i-1] = result[i];
-    			result[i] = t;
+	  	   result[i+1] = a[0];
+	   	else{
+	 	   int t = result[i-1];
+	   	   result[i-1] = result[i];
+    		   result[i] = t;
 		
-    			if(result[i]==a[0])
-	   		      result[i+1] = a[1];
-	        	else if(result[i]==a[1])
-		          result[i+1] = a[0];
-	   		}
-	    }
+    		   if(result[i]==a[0])
+	   	      result[i+1] = a[1];
+	           else if(result[i]==a[1])
+		      result[i+1] = a[0];
+	   	}
+	    }  
 	   	
-		if(i==1)
-            i++; //Because of queries
+	    if(i==1)
+               i++; //Because of queries
               
-    }
+        }
     
-    //Output
-    for(int i=0; i<6; i++){
+        //Output
+        for(int i=0; i<6; i++){
 	    printf("%d ",result[i]);
 	    fflush(stdout);
 	}
-		
-}
+    		
+} 
 	
